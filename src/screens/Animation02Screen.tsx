43809fccbeaa029d73027159a33debb97df2a558
Esteban Canteros <estebanacanteros@gmail.com>
2021-06-26 21:56:10 -0300

    pan responder animation

--- src/screens/Animation02Screen.tsx
+++ src/screens/Animation02Screen.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, {useRef} from 'react'
+import { View, PanResponder, Animated } from 'react-native'
 import styles from '../themes/AppTheme'
 
 const Animation02Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      { dx: pan.x, dy: pan.y, }, 
+      ], 
+      {useNativeDriver: false }
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan,
+        {toValue: {x: 0, y: 0}, useNativeDriver: false},
+      ).start();
+    },
+  })
+
   return (
     <View style={styles.containerCenter}>
-      <View style={styles.purpleBox} />
+      <Animated.View 
+      {...panResponder.panHandlers}
+      style={[pan.getLayout(), styles.purpleBox, {backgroundColor: '#61dafb'}]} 
+      />
     </View>
   )
 }
