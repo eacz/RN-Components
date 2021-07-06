62dc334d89983a5bdfaad9b5edc06b02358c2488
Esteban Canteros <estebanacanteros@gmail.com>
2021-07-06 19:42:18 -0300

    Alert Prompt

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { View, Button, Alert } from 'react-native'
+import { View, Button, Alert, Platform, Text } from 'react-native'
+import prompt from 'react-native-prompt-android';
+
 import HeaderTitle from '../components/HeaderTitle'
 import styles from '../themes/AppTheme'
 
@@ -11,11 +13,40 @@ const AlertScreen = () => {
     ],
     //{cancelable: true, onDismiss: () => console.log('dismissed')}
     )
+    
   }
+  const showPrompt = () => {
+    //IOS ONLY
+    //Alert.prompt(
+    //  'Prompt', 
+    //  'Prompt message', 
+    //  (text) => console.log(text),
+    //  'default',
+    //  'nothing',
+    //  'number-pad'
+    //)
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+       {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+       {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
+      ],
+      {
+          type: 'secure-text',
+          cancelable: false,
+          defaultValue: 'test',
+          placeholder: 'placeholder'
+      }
+  );
+  }
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
       <Button title="Show Alert" onPress={showAlert} />
+
+      <Button title="Show Prompt" onPress={showPrompt} />
     </View>
   )
 }
