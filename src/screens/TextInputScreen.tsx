88b925b1ac42fd5e9f8b4ed35885dbf840af9600
Esteban Canteros <estebanacanteros@gmail.com>
2021-07-06 22:04:28 -0300

    useForm hook to manage forms

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,63 +1,85 @@
 import React from 'react'
 import { useState } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native'
+import { View, TextInput, StyleSheet, KeyboardAvoidingView, Platform, ScrollView, Text } from 'react-native';
+import CustomSwitch from '../components/CustomSwitch';
 import HeaderTitle from '../components/HeaderTitle';
+import useForm from '../hooks/useForm';
 import stylesTheme from '../themes/AppTheme';
 
 const TextInputScreen = () => {
-  const [form, setForm] = useState({
+  const formInitState = {
     name: '',
     email: '',
-    phone: ''
-  })
-
-  const handleChange = (text: string, field: keyof typeof form) => {
-    setForm({...form, [field]:text})
+    phone: '',
+    isSubscribed: false
   }
+  const {form, onChange} = useForm(formInitState);
 
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="Text Inputs" />
-      <TextInput 
-        style={styles.textInput}
-        placeholder="Write your name"
-        placeholderTextColor="gray"
-        autoCorrect={false}
-        autoCapitalize='words'
-        onChangeText={(text) => handleChange(text, 'name')}
-      />
-      <TextInput 
-        style={styles.textInput}
-        placeholder="Write your email"
-        placeholderTextColor="gray"
-        autoCorrect={false}
-        autoCapitalize='none'
-        onChangeText={(text) => handleChange(text, 'email')}
-        keyboardType='email-address'
-      />
-      <TextInput 
-        style={styles.textInput}
-        placeholder="Write your phone"
-        placeholderTextColor="gray"
-        autoCorrect={false}
-        onChangeText={(text) => handleChange(text, 'phone')}
-        keyboardType="number-pad"
-      />
-      <HeaderTitle title={JSON.stringify(form, null, 3)} />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    > 
+      <ScrollView>
+        <View style={styles.globalMargin}>
+          <HeaderTitle title="Text Inputs" />
+          <TextInput 
+            style={styles.textInput}
+            placeholder="Write your name"
+            placeholderTextColor="gray"
+            autoCorrect={false}
+            autoCapitalize='words'
+            onChangeText={(text) => onChange(text, 'name')}
+            />
+          <TextInput 
+            style={styles.textInput}
+            placeholder="Write your email"
+            placeholderTextColor="gray"
+            autoCorrect={false}
+            autoCapitalize='none'
+            onChangeText={(text) => onChange(text, 'email')}
+            keyboardType='email-address'
+            />
+
+          <TextInput 
+            style={styles.textInput}
+            placeholder="Write your phone"
+            placeholderTextColor="gray"
+            autoCorrect={false}
+            onChangeText={(text) => onChange(text, 'phone')}
+            keyboardType="number-pad"
+            />
+
+            <View style={styles.switchRow}>
+              <Text style={styles.switchText}>Subscribe: </Text>
+              <CustomSwitch onChange={(value) => onChange(value, 'isSubscribed')} isOn={form.isSubscribed}  />
+            </View>
+            <HeaderTitle title={JSON.stringify(form, null, 2)} />
+            
+        </View>
+    </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 const styles = StyleSheet.create({
-    ...stylesTheme,
-    textInput: {
-      borderWidth: 2,
-      height: 50,
-      paddingHorizontal: 10,
-      marginVertical: 10,
-      borderColor: 'rgba(0,0,0, 0.5)',
+  ...stylesTheme,
+  textInput: {
+    borderWidth: 2,
+    height: 50,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    borderColor: 'rgba(0,0,0, 0.5)',
       color: '#000000',
       borderRadius: 10,
-    }
+  },
+  switchText: {
+    fontSize: 25
+  },
+  switchRow: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'space-between'
+  }
 });
 
 export default TextInputScreen
